d9eba5cd5bbc6a75f9d04f30060903fa7abd0979
muujii1015 <munkhjargal.lkhagvasuren@griffithuni.edu.au>
2024-03-23 15:05:41 +1000

    Update App.js
    Milestone 1

--- assignment1/App.js
+++ assignment1/App.js
@@ -1,29 +1,92 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet , Pressable} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headertext}>My Todo List</Text>
+       
+        <View style={styles.line}></View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.listitem}>
+            <Text style={styles.listItemText}>Buy Milk</Text>
+         </View>
+         <View style={styles.listitem}>
+            <Text style={styles.listItemText}>Buy Bread</Text>
+         </View>
+         <View style={styles.listitem}>
+            <Text style={styles.listItemText}>Buy Eggs</Text>
+         </View>
+     
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.line}></View>
+        <Pressable style={({ pressed }) => [styles.button,{ backgroundColor: pressed ? 'blue' : '#3498db' }]}  onPress={() => console.log("Button pressed")}>
+          <Text style={styles.buttonText}>Add New Todo</Text>
+        </Pressable>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    flexDirection:'column',
+    backgroundColor: 'white',
+    padding: 20,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
+  header:{
+    flex:2,
+    justifyContent:'center',
+    alignItems:'center',
+  
+  },
+  headertext:{
+    fontSize: 30,
     fontWeight: 'bold',
-    textAlign: 'center',
+    padding:10,
   },
+  body:{
+    flex:7,
+  
+    alignItems:'center',
+  },
+  line: {
+    width:'100%',
+    height: 2, 
+    backgroundColor: '#000', 
+  },
+  listitem: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#3498db', 
+    borderRadius: 5,
+    justifyContent: 'center',
+   
+    marginVertical: 10,
+    paddingHorizontal: 20,
+  },
+  listItemText: {
+    color: 'white',
+    fontSize: 16, 
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    marginTop: 30, 
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  
 });
